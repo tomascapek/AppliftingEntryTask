66c3674e420dee4760abe3061d07db90acbeb991
Tomáš Čapek <tomas@capek.io>
2021-07-22 20:58:01 +0200

    Remove unused imports from database.py.

--- database.py
+++ database.py
@@ -1,7 +1,5 @@
-import enum
-
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Enum, create_engine
-from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
