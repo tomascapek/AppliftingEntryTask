e1b651562a5390eca0f705f3abf8779625837156
Tomáš Čapek <tomas@capek.io>
2021-07-22 20:28:19 +0200

    Fix wrongly created db init.

--- database.py
+++ database.py
@@ -8,6 +8,15 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from database import engine, session
+from database import engine, get_db
 import model
 
 model.Base.metadata.create_all(bind=engine)
+
+db = get_db()
 
 if __name__ == '__main__':
     print("Hello world!")
